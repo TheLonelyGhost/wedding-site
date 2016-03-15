58d674ccaf86cd86b3fcf9784a7fe4217591ecad
David Alexander <davidpaulalexander@gmail.com>
2016-03-15 09:42:11 -0400

    Wrap JS in closure for sandboxing

--- js/base.js
+++ js/base.js
@@ -1,8 +1,10 @@
-var clock = document.getElementById('wedding-date'),
-  targetDate = new Date(clock.getAttribute('datetime')),
-  updateCountdown = function() {
-    clock.innerHTML = countdown(null, targetDate, countdown.YEARS|countdown.MONTHS|countdown.WEEKS|countdown.DAYS/*|countdown.HOURS|countdown.MINUTES|countdown.SECONDS*/).toString();
-  };
-countdown.setLabels(null, null, ', and ');
-updateCountdown();
-setInterval(updateCountdown, 6000);
+;(function(countdown) {
+  var clock = document.getElementById('wedding-date'),
+    targetDate = new Date(clock.getAttribute('datetime')),
+    updateCountdown = function() {
+      clock.innerHTML = countdown(null, targetDate, countdown.YEARS|countdown.MONTHS|countdown.WEEKS|countdown.DAYS/*|countdown.HOURS|countdown.MINUTES|countdown.SECONDS*/).toString();
+    };
+  countdown.setLabels(null, null, ', and ');
+  updateCountdown();
+  setInterval(updateCountdown, 6000);
+})(window.countdown);
