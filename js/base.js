fe4ad515d2c4d9ce13bc0838fc31b2978d062ad1
David Alexander <davidpaulalexander@gmail.com>
2016-03-16 10:16:36 -0400

    Adds RSVP page and menu
    Fixes bugs from assuming single-page website, e.g. countdown timer
    element not existing on all pages.

--- js/base.js
+++ js/base.js
@@ -3,13 +3,21 @@
     window.location = window.location.protocol + "//alexander-wedding.me";
   }
 })();
+(function($) {
+  $('.menu a.current').on('click', function(e) {
+    e.preventDefault();
+  });
+})(window.jQuery);
 (function(countdown) {
-  var clock = document.getElementById('wedding-date'),
-    targetDate = new Date(clock.getAttribute('datetime')),
-    updateCountdown = function() {
-      clock.innerHTML = countdown(null, targetDate, countdown.YEARS|countdown.MONTHS|countdown.WEEKS|countdown.DAYS/*|countdown.HOURS|countdown.MINUTES|countdown.SECONDS*/).toString();
-    };
-  countdown.setLabels(null, null, ', and ');
-  updateCountdown();
-  setInterval(updateCountdown, 6000);
+  var clock = document.getElementById('wedding-date');
+
+  if(clock) {
+    var targetDate = new Date(clock.getAttribute('datetime')),
+      updateCountdown = function() {
+        clock.innerHTML = countdown(null, targetDate, countdown.YEARS|countdown.MONTHS|countdown.WEEKS|countdown.DAYS/*|countdown.HOURS|countdown.MINUTES|countdown.SECONDS*/).toString();
+      };
+    countdown.setLabels(null, null, ', and ');
+    updateCountdown();
+    setInterval(updateCountdown, 6000);
+  }
 })(window.countdown);
